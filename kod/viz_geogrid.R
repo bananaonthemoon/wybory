91c29399186461e022bfd4cc4f84918b524867ce
Maria <bananaonthemoon@users.noreply.github.com>
2020-04-21 13:56:57 +0200

    dodanie wizualizacji biblioteka 'geogrid'

--- kod/viz_geogrid.R
+++ kod/viz_geogrid.R
@@ -3,23 +3,20 @@ library(sf)
 library(tidyverse)
 library(tmap)
 
-#download.file("https://www.gis-support.pl/downloads/Wojewodztwa.zip", "Wojewodztwa.zip")
-#unzip("Wojewodztwa.zip")
-granice = read_sf("Wojew˘dztwa.shp", stringsAsFactors=FALSE) %>%
-  st_transform(crs = 2180) %>%
-  st_combine() 
+prez = "https://raw.github.com/bananaonthemoon/wybory/master/dane/prez_woj.gpkg"
+prez_woj = read_sf(prez, stringsAsFactors=FALSE) 
+prez_woj$Nazwa4 <- substr(prez_woj$Nazwa, 1, 4)
 
-prez_woj = read_sf("prez_woj.gpkg", stringsAsFactors=FALSE) 
-prez_woj$nazwa4 <- substr(prez_woj$Nazwa, 1, 4)
+
+# rawplot -----------------------------------------------------------------
+
 
 rawplot <- tm_shape(prez_woj) + 
-  tm_polygons("X1_frekw", palette = "viridis") +
-  tm_text("nazwa4")
-rawplot
+  tm_polygons("f1.duda", palette = "viridis") +
+  tm_text("Nazwa4")
 
 
-
-# hex ---------------------------------------------------------------------
+# hexplot -----------------------------------------------------------------
 
 
 par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
@@ -31,8 +28,12 @@ for (i in 1:6) {
 new_cells_hex <- calculate_grid(shape = prez_woj, grid_type = "hexagonal", seed = 2)
 resulthex <- assign_polygons(prez_woj, new_cells_hex)
 
+hexplot <- tm_shape(resulthex) + 
+  tm_polygons("t1_Andrzej.Sebastian.Duda", palette = "viridis") +
+  tm_text("Nazwa4")
 
-# reg ---------------------------------------------------------------------
+
+# regplot -----------------------------------------------------------------
 
 par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
 for (i in 1:6) {
@@ -43,15 +44,9 @@ for (i in 1:6) {
 new_cells_reg <- calculate_grid(shape = prez_woj, grid_type = "regular", seed = 2)
 resultreg <- assign_polygons(prez_woj, new_cells_reg)
 
-
-# laczenie wizualizacji ---------------------------------------------------
-
-hexplot <- tm_shape(resulthex) + 
-  tm_polygons("X1_frekw", palette = "viridis") +
-  tm_text("nazwa4")
-
 regplot <- tm_shape(resultreg) + 
-  tm_polygons("X1_frekw", palette = "viridis") +
-  tm_text("nazwa4")
+  tm_polygons("f1.duda", palette = "viridis") +
+  tm_text("Nazwa4")
 
 tmap_arrange(rawplot, hexplot, regplot, nrow = 3)
+
