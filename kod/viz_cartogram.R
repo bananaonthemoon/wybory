79e924872605ff39f019f7bcde85b1bc198856f8
Maria <bananaonthemoon@users.noreply.github.com>
2020-04-25 17:21:44 +0200

    wersja z mniejszą liczbą kolumn

--- kod/viz_cartogram.R
+++ kod/viz_cartogram.R
@@ -9,17 +9,18 @@ prez = "https://raw.github.com/bananaonthemoon/wybory/master/dane/prez_powiaty.g
 prez_woj = read_sf(prez, stringsAsFactors=FALSE) %>%
   st_transform(crs = 2180)
 
-#granice = read_sf(prez, stringsAsFactors=FALSE) %>%
-#  st_transform(crs = 2180) %>%
-#  st_union() 
-
 download.file("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_subunits.zip", "dane/temp/ne_10m_admin_0_map_subunits.zip")
 unzip("dane/temp/ne_10m_admin_0_map_subunits.zip", exdir = "dane/pobrane")
 
-sasiad = c("Germany", "Czechia", "Slovakia", "Ukraine", "Belarus", "Lithuania", "Kalinigrad", "Poland")
+sasiad = c("Germany", "Czechia", "Slovakia", "Ukraine", "Belarus", "Lithuania", "Kalinigrad", "Austria")
 
 bord = read_sf("dane/pobrane/ne_10m_admin_0_map_subunits.shp") %>%
   filter(SUBUNIT %in% sasiad) %>%
+  select(NAME, POSTAL) %>%
+  st_transform(crs = 2180)
+
+granica = read_sf("dane/pobrane/ne_10m_admin_0_map_subunits.shp") %>%
+  filter(SUBUNIT == "Poland") %>%
   select(NAME, POSTAL) %>%
   st_transform(crs = 2180)
 
@@ -32,30 +33,46 @@ prez_woj_ncont = cartogram_ncont(prez_woj, "f1.duda")
 # Non-overlapping Circles Cartogram
 prez_woj_dorling = cartogram_dorling(prez_woj, "f1.duda")
 
-#xlim = c(min(prez_woj$X), max(prez_woj$X)), ylim= c(min(prez_woj$Y), max(prez_woj$Y)))
 
 # Plots
-tm_shape(bord) + 
-  tm_borders() +
+
+tmaptools::palette_explorer()
+
+# Bounding box dla Polski
+bb = bb(prez_woj, ext = 1.05)
+
+paleta_duda = get_brewer_pal("YlOrRd", n = 5, contrast = c(0.15, 0.7))
+
+# Continuous Area Cartogram
+map_1 = tm_shape(granica, bbox = bb) +
+  tm_polygons(border.col = "white", col = "gray85") +
+tm_shape(bord) +
+  tm_polygons(border.col = "white", col = "gray85") +
 tm_shape(prez_woj_cont) +
-  tm_polygons("f1.duda", style = "jenks", legend.show = FALSE) +
+  tm_polygons("f1.duda", style = "jenks", border.col = "White", border.alpha = 0.1, palette = paleta_duda) +
+tm_scale_bar(position = c("left", "bottom")) +
+tm_layout(bg.color = "lightcyan2")
 
-last_plot()
+map_2 = tm_shape(granica, bbox = bb) +
+tm_polygons(border.col = "white", col = "gray85") +
+  tm_shape(bord) +
+  tm_polygons(border.col = "white", col = "gray85") +
+tm_shape(prez_woj_ncont) +
+  tm_polygons("f1.duda", style = "jenks", border.col = "White", border.alpha = 0.1, palette = paleta_duda) +
+tm_scale_bar(position = c("left", "bottom")) +
+tm_layout(bg.color = "lightcyan2")
 
-+
-  tm_shape(prez_woj_cont) + tm_polygons("f1.duda", style = "jenks", legend.show = FALSE) +
-  tm_layout(frame = FALSE)
-plot(m1)
-
-m2 = tm_shape(prez_woj) + tm_borders() + 
-  tm_shape(prez_woj_ncont) + tm_polygons("f1.duda", style = "jenks", legend.show = FALSE) +
-  tm_layout(frame = FALSE)
-
-m3 = tm_shape(prez_woj) + tm_borders() + 
-  tm_shape(prez_woj_dorling) + tm_polygons("f1.duda", style = "jenks", legend.show = FALSE) +
-  tm_layout(frame = FALSE, legend.outside = TRUE)
+#map_3 = 
+tm_shape(granica, bbox = bb) +
+  tm_polygons(border.col = "white", col = "gray85") +
+tm_shape(bord) +
+  tm_polygons(border.col = "white", col = "gray85") +
+tm_shape(prez_woj_dorling) +
+  tm_polygons("f1.duda", style = "jenks", border.col = "White", border.alpha = 0.1, palette = paleta_duda) +
+tm_scale_bar(position = c("left", "bottom")) +
+tm_layout(bg.color = "lightcyan2")
 
 ml = tm_shape(prez_woj_dorling) + tm_polygons("f1.duda", style = "jenks") +
   tm_layout(frame = FALSE, legend.only = TRUE, legend.position = c("center", "center"))
 
-tmap_arrange(m1, m2, m3, ml, nrow = 1)
+#tmap_arrange(map_1, map_2, map_3, ml, nrow = 1)
