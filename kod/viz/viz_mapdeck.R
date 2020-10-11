b6ff326a093974cd3ea28110d4760bb5cd9167b5
Maria <61518849+bananaonthemoon@users.noreply.github.com>
2020-10-11 12:37:55 +0200

    viz_mapdeck

--- kod/viz/viz_mapdeck.R
+++ kod/viz/viz_mapdeck.R
@@ -15,3 +15,13 @@ mapdeck(token = key)
 prez_woj = "https://raw.github.com/bananaonthemoon/wybory/master/dane/powiaty.gpkg" %>%
   read_sf() %>%
   st_transform(crs = 4326)
+
+map_woj = st_centroid(prez_woj)
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_hexagon(
+    data = map_woj,
+    radius = 5000,
+    elevation ="t1_Frekwencja",
+    elevation_scale = 10
+  )
