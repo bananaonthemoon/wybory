e33b0d70784fc1897ea28f54635ff195cc6feafa
Maria <61518849+bananaonthemoon@users.noreply.github.com>
2020-10-21 13:27:10 +0200

    21.10

--- kod/viz/viz_geogrid.R
+++ kod/viz/viz_geogrid.R
@@ -3,54 +3,98 @@ library(sf)
 library(tidyverse)
 library(tmap)
 
-#uzupelnic o powiaty i wielkopolske
+#wizualizacja na przykładzie województw
+
 # WOJEWODZTWA -------------------------------------------------------------
 
-# Dane
-
-prez = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wojewodztwa.gpkg"
-prez_woj = read_sf(prez, stringsAsFactors=FALSE) 
-prez_woj$Nazwa4 <- substr(prez_woj$Nazwa, 1, 4)
+wojewodztwa = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wojewodztwa.gpkg"
+woj = read_sf(wojewodztwa, stringsAsFactors=FALSE) 
+woj$Nazwa4 = substr(woj$JPT_NAZWA_, 1, 4)
 
 
-# obliczanie rawplot
-
-rawplot <- tm_shape(prez_woj) + 
+# rawplot
+rawplot = tm_shape(woj) + 
   tm_polygons("f1.duda", palette = "viridis") +
   tm_text("Nazwa4")
 
 
-# obliczanie hexplot
-
+# hexplot
 par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
 for (i in 1:6) {
-  new_cells <- calculate_grid(shape = prez_woj, grid_type = "hexagonal", seed = i)
+  new_cells = calculate_grid(shape = woj, grid_type = "hexagonal", seed = i)
   plot(new_cells, main = paste("Seed", i, sep = " "))
 }
 
-new_cells_hex <- calculate_grid(shape = prez_woj, grid_type = "hexagonal", seed = 2)
-resulthex <- assign_polygons(prez_woj, new_cells_hex)
+new_cells_hex = calculate_grid(shape = woj, grid_type = "hexagonal", seed = 1)
+resulthex <- assign_polygons(woj, new_cells_hex)
 
-hexplot <- tm_shape(resulthex) + 
+hexplot = tm_shape(resulthex) + 
   tm_polygons("t1_Andrzej.Sebastian.Duda", palette = "viridis") +
   tm_text("Nazwa4")
 
 
-# obliczanie regplot
-
+# regplot
 par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
 for (i in 1:6) {
-  new_cells <- calculate_grid(shape = prez_woj, grid_type = "regular", seed = i)
+  new_cells <- calculate_grid(shape = woj, grid_type = "regular", seed = i)
   plot(new_cells, main = paste("Seed", i, sep = " "))
 }
 
-new_cells_reg <- calculate_grid(shape = prez_woj, grid_type = "regular", seed = 2)
-resultreg <- assign_polygons(prez_woj, new_cells_reg)
+new_cells_reg = calculate_grid(shape = woj, grid_type = "regular", seed = 2)
+resultreg <- assign_polygons(woj, new_cells_reg)
 
-regplot <- tm_shape(resultreg) + 
+regplot = tm_shape(resultreg) + 
   tm_polygons("f1.duda", palette = "viridis") +
   tm_text("Nazwa4")
 
 # plot
-
 tmap_arrange(rawplot, hexplot, regplot, nrow = 3)
+
+
+#czy dodawać też powiaty i wielkopolskę?
+# POWIATY -----------------------------------------------------------------
+
+
+powiaty = "https://raw.github.com/bananaonthemoon/wybory/master/dane/powiaty.gpkg"
+pow = read_sf(prez, stringsAsFactors=FALSE) 
+pow$Nazwa4 = substr(pow$Nazwa, 1, 4)
+
+
+# rawplot
+rawplot = tm_shape(pow) + 
+  tm_polygons("f1.duda", palette = "viridis") +
+  tm_text("Nazwa4")
+
+
+# hexplot
+par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
+for (i in 1:6) {
+  new_cells = calculate_grid(shape = pow, grid_type = "hexagonal", seed = i)
+  plot(new_cells, main = paste("Seed", i, sep = " "))
+}
+
+new_cells_hex = calculate_grid(shape = pow, grid_type = "hexagonal", seed = 2)
+resulthex = assign_polygons(pow, new_cells_hex)
+
+hexplot = tm_shape(resulthex) + 
+  tm_polygons("t1_Andrzej.Sebastian.Duda", palette = "viridis") +
+  tm_text("Nazwa4")
+
+
+# regplot
+par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
+for (i in 1:6) {
+  new_cells = calculate_grid(shape = pow, grid_type = "regular", seed = i)
+  plot(new_cells, main = paste("Seed", i, sep = " "))
+}
+
+new_cells_reg = calculate_grid(shape = pow, grid_type = "regular", seed = 2)
+resultreg <- assign_polygons(pow, new_cells_reg)
+
+regplot = tm_shape(resultreg) + 
+  tm_polygons("f1.duda", palette = "viridis") +
+  tm_text("Nazwa4")
+
+# plot
+tmap_arrange(rawplot, hexplot, regplot, nrow = 3)
+
--- kod/viz/viz_mapdeck.R
+++ kod/viz/viz_mapdeck.R
@@ -3,25 +3,160 @@ library(tidyverse)
 library(sf)
 library(st)
 library(tmaptools)
+library(colourvalues)
+
+#wizualizacja hex
+
+# add_hexagon() -------------------------------------------------------------
 
 
-# WOJEWODZTWA -------------------------------------------------------------
+# WOJEWODZTWA
 
 
 key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
 
-mapdeck(token = key)
-
-prez_woj = "https://raw.github.com/bananaonthemoon/wybory/master/dane/powiaty.gpkg" %>%
+prez_woj = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wojewodztwa.gpkg" %>%
   read_sf() %>%
   st_transform(crs = 4326)
 
-map_woj = st_centroid(prez_woj)
+hex_woj = st_centroid(prez_woj)
+hex_woj$frek = prez_woj$t1_Frekwencja ^2 #nie ma sensu tutej używać
 
 mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
   add_hexagon(
-    data = map_woj,
-    radius = 5000,
+    data = hex_woj,
+    radius = 25000,
     elevation ="t1_Frekwencja",
-    elevation_scale = 10
+    elevation_scale = 100,
+    fill_colour = "t1_Frekwencja",
+    palette = "oranges",
+    colour_range = colour_values(1:6, palette = "orrd"), #kolor nie działa?
+    legend = TRUE #value to same 111111
+  )
+
+
+
+# POWIATY
+
+
+key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
+
+prez_pow = "https://raw.github.com/bananaonthemoon/wybory/master/dane/powiaty.gpkg" %>%
+  read_sf() %>%
+  st_transform(crs = 4326)
+
+hex_woj = st_centroid(prez_pow)
+hex_woj$frek = prez_pow$t1_Frekwencja ^2 #nie ma sensu tutej używać
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_hexagon(
+    data = prez_pow,
+    radius = 25000,
+    elevation ="t1_Frekwencja",
+    elevation_scale = 100,
+    fill_colour = "t1_Frekwencja",
+    palette = "oranges",
+    colour_range = colour_values(1:6, palette = "orrd"), #kolor nie działa?
+    legend = TRUE #value to same 111111
+  )
+
+
+
+# WIELKOPOLSKA
+
+
+key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
+
+prez_wlkp = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wielkopolska.gpkg" %>%
+  read_sf() %>%
+  st_transform(crs = 4326)
+
+hex_woj = st_centroid(prez_wlkp)
+hex_woj$frek = prez_wlkp$t1_Frekwencja ^2 #nie ma sensu tutej używać
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_hexagon(
+    data = prez_wlkp,
+    radius = 25000,
+    elevation ="t1_Frekwencja",
+    elevation_scale = 100,
+    fill_colour = "t1_Frekwencja",
+    palette = "oranges",
+    colour_range = colour_values(1:6, palette = "orrd"), #kolor nie działa?
+    legend = TRUE #value to same 111111
+  )
+
+
+
+
+#wizualizacja poly
+
+# add_polygon() -----------------------------------------------------------
+
+
+# WOJEWODZTWA 
+
+
+key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
+
+poly_woj = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wojewodztwa.gpkg" %>%
+  read_sf() %>%
+  st_transform(crs = 4326)
+
+
+poly_woj$frek = prez_woj$t1_Frekwencja ^2 #też nie idzie
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_polygon(
+    data = poly_woj,
+    fill_colour = "t1_Frekwencja",
+    elevation ="frek",
+    elevation_scale = 100,
+    palette = "oranges"
+  )
+
+
+
+# POWIATY 
+
+
+key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
+
+poly_pow = "https://raw.github.com/bananaonthemoon/wybory/master/powiaty.gpkg" %>%
+  read_sf() %>%
+  st_transform(crs = 4326)
+
+
+poly_pow$frek = poly_pow$t1_Frekwencja ^2 #też nie idzie
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_polygon(
+    data = poly_pow,
+    fill_colour = "t1_Frekwencja",
+    elevation ="frek",
+    elevation_scale = 100,
+    palette = "oranges"
+  )
+
+
+
+# WIELKOPOLSKA 
+
+
+key = 'pk.eyJ1IjoibWFya3JvOCIsImEiOiJja2QwODBzdGEwOGpwMnpvMThwdmFqZjNyIn0.BltIhc4-ZufZxHhOyUo30g'
+
+poly_wlkp = "https://raw.github.com/bananaonthemoon/wybory/master/wielkopolska.gpkg" %>%
+  read_sf() %>%
+  st_transform(crs = 4326)
+
+
+poly_wlkp$frek = poly_wlkp$t1_Frekwencja ^2 #też nie idzie
+
+mapdeck(token = key, style = mapdeck_style('light'),pitch = 45 ) %>%
+  add_polygon(
+    data = poly_wlkp,
+    fill_colour = "t1_Frekwencja",
+    elevation ="frek",
+    elevation_scale = 100,
+    palette = "oranges"
   )
