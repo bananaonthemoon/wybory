696dc07c7149accaaa87433ad54d631ac1e97fcb
Maria <61518849+bananaonthemoon@users.noreply.github.com>
2020-11-13 16:10:22 +0100

    13.11

--- kod/viz/viz_rayshader.R
+++ kod/viz/viz_rayshader.R
@@ -2,20 +2,24 @@ library(sf)
 library(rayshader)
 library(tidyverse)
 library(RColorBrewer)
+library(classInt)
 
-prez_w = "https://raw.github.com/bananaonthemoon/wybory/master/dane/prez_woj_demo.gpkg"
+prez_w = "https://raw.github.com/bananaonthemoon/wybory/master/dane/wojewodztwa.gpkg"
 prez_woj = read_sf(prez_w, stringsAsFactors=FALSE)
 
-paleta_blue = display.brewer.pal(5, "Blues")
+
+#coś takiego zrobiłam, ale nie wiem czy nie znaleźć czegoś innego?
+frek1_klasy <- classIntervals(prez_woj$t1_Frekwencja, n = 5, style = "jenks")
+prez_woj = prez_woj %>% mutate(frek1 = cut(t1_Frekwencja, frek1_klasy$brks, include.lowest = T))
                
 theme_set(theme_bw())
 
 gg_prez_woj = ggplot(prez_woj) +
-  geom_sf(aes(fill = t1.komo), color = "white", size = 0.4) +
-  scale_colour_brewer(palette = "Blues", direction = -1) +
-  ggtitle("Rayshader")
+  geom_sf(aes(fill = t1_Frekwencja), color = "white", size = 0.4) +
+  scale_fill_fermenter("Frekwencja [%]", palette = "Greens", direction = 1) + #ferment
+  ggtitle("Frekwencja w pierwszej turze \nwyborów prezydenckich w 2020 r.")
 
 plot(gg_prez_woj)
 
-plot_gg(gg_prez_woj, shadow_intensity = 0.2, fov = 70)
+plot_gg(gg_prez_woj, width = 5, height = 4, scale = 100, multicore = TRUE, shadow_intensity = 0.5, preview = TRUE)
 render_depth(focallength=100,focus=0.72)
